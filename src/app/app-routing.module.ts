6fd254d70cae2fb4f252f37f9a2732f596b84c3b
Nguyễn Lương Bách <bachnguyen120195@gmail.com>
2023-04-14 16:51:12 +0700

    Add lazy loading and environment variables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './authenticate/auth/auth.component';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'', redirectTo: '/recipes', pathMatch:'full'},
-  {path: 'auth', component: AuthComponent}
+  {path:'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
+  {path:'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)},
+  {path:'auth', loadChildren: () => import('./authenticate/auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './authenticate/auth/auth.component';
 import { SharedModule } from './shared/shared.module';
-import { FormsModule } from '@angular/forms';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    FormsModule,
-    ShoppingListModule,
-    RecipesModule,
     CoreModule
   ],
   bootstrap: [AppComponent]
