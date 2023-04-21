4d534ca846911f90e25448b5adbcfbf2cee579aa
Nguyễn Lương Bách <bachnguyen120195@gmail.com>
2023-04-21 09:18:31 +0700

    remove unessesary code in datastorage

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,39 +1,38 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { exhaustMap, map, take, tap } from "rxjs";
-import { AuthService } from "../authenticate/auth/auth.service";
+import { map, tap } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorage {
-    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
 
     storeRecipes() {
-      const recipes = this.recipeService.getRecipes();
+        const recipes = this.recipeService.getRecipes();
 
-      this.http.put('https://angular-course-7ad3a-default-rtdb.firebaseio.com/recipes.json', recipes)
-          .subscribe(
-              response => {
-                  console.log(response);
-              }
-          );
+    this.http.put('https://angular-course-7ad3a-default-rtdb.firebaseio.com/recipes.json', recipes)
+        .subscribe(
+            response => {
+                console.log(response);
+            }
+        );
     }
 
     fetchRecipes() {
-      return this.http.get<Recipe[]>('https://angular-course-7ad3a-default-rtdb.firebaseio.com/recipes.json').pipe(
-        map(recipes => {
-          return recipes.map(
-              recipe => {
-                  return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-              }
-          )
-        }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        })
-      );
+        return this.http.get<Recipe[]>('https://angular-course-7ad3a-default-rtdb.firebaseio.com/recipes.json').pipe(
+            map(recipes => {
+            return recipes.map(
+                recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                }
+            )
+            }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
+        );
     }
 }
